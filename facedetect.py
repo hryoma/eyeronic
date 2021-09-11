df7e0bb96469fa0c262f2135544d265d300768aa
ryoma <84463842+hryoma@users.noreply.github.com>
2021-09-11 17:18:38 -0400

    Connected face detect to website

--- facedetect.py
+++ facedetect.py
@@ -1,27 +1,28 @@
-import numpy as np
 import cv2
-from PIL import Image, ImageDraw, ImageOps
+from PIL import Image, ImageOps
+
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
-#save the image(i) in the same directory
-img = cv2.imread("family.jpg")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-count = 0
-for (x,y,w,h) in faces:
-	img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-	roi_gray = gray[y:y+h, x:x+w]
-	roi_color = img[y:y+h, x:x+w]
-	eyes = eye_cascade.detectMultiScale(roi_gray)
-	for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-            im = Image.open('family.jpg')
-            print(ex,ey,ew+ex,ey+eh)
-            im = im.crop((ex+x,ey+y,ex+ew+x, ey+eh+y))
-            im = ImageOps.grayscale(im)
-            im = im.resize((80,80))
-            im.save('croppedeyes'+str(count) +  ".jpg")
-            count += 1
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+
+def find_eyes(filepath):
+    img = cv2.imread(filepath)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    eye_count = 0
+
+    for (x, y, w, h) in faces:
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+            img_eye = Image.open(filepath)
+            img_eye = img_eye.crop((ex + x, ey + y, ex + ew + x, ey + eh + y))
+            img_eye = ImageOps.grayscale(img_eye)
+            img_eye = img_eye.resize((80, 80))
+            img_eye.save('eyes/cropped-eyes-' + str(eye_count) + ".jpg")
+            eye_count += 1
+
