2e6bf2a9af6fb57034480e740df581108cdecbe8
ryoma <84463842+hryoma@users.noreply.github.com>
2021-09-11 23:02:16 -0400

    fixed model loading

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ import io
 import pathlib
 
 import facedetect
+import tensorflow as tf
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import dropout, fully_connected
+from tflearn.layers.estimator import regression
+import numpy as np
+from PIL import Image
+from skimage import transform
 
 app = Flask(__name__)
 dropzone = Dropzone(app)
@@ -39,14 +47,74 @@ def index():
 
 @app.route('/download')
 def download():
-    base_path = pathlib.Path('inputs')
+    inputs_path = pathlib.Path('inputs')
+    eyes_path = pathlib.Path('eyes')
     data = io.BytesIO()
 
+    # set up DNN
+    tf.compat.v1.reset_default_graph()
+    convnet = tflearn.input_data(shape=[None, 50, 50, 1], name='input')
+
+    convnet = conv_2d(convnet, 32, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 64, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 128, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 64, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 32, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = fully_connected(convnet, 1024, activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, 2, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=1e-3,
+                         loss='categorical_crossentropy', name='targets')
+
+    model = tflearn.DNN(convnet, tensorboard_dir='log')
+    model.load('EyeDet.h5')
+
+    # process each input image
+    for ind, filepath in enumerate(inputs_path.iterdir()):
+        facedetect.find_eyes(str(filepath))
+
+    for image_dir in eyes_path.iterdir():
+        keep_img = False
+        print(str(image_dir))
+
+        # todo: not sure if this part works yet
+        # process each eye for the input image
+        for eyes_path in image_dir.iterdir():
+            eyes_img = Image.open(str(eyes_path))
+            eyes_img = np.array(eyes_img).astype('float32') / 255
+            eyes_img = transform.resize(eyes_img, (50, 50, 1))
+            eyes_img = np.expand_dims(eyes_img, axis=0)
+
+            prediction = model.predict(eyes_img)[0]
+            if prediction[0] > prediction[1]:
+                # opened - keeping looking for a closed eye
+                continue
+            else:
+                # closed - can put image in output
+                keep_img = True
+                break
+
+        # remove the image with only open eyes from the inputs
+        if not keep_img:
+            os.remove('inputs/' + str(image_dir))  # todo: figure out the right path
+
     with zipfile.ZipFile(data, mode='w') as zip_output:
-        for file_path in base_path.iterdir():
-            facedetect.find_eyes(str(file_path))
+        for file_path in inputs_path.iterdir():
             zip_output.write(file_path)
-    data.seek(0)
+
+    # clean up - delete all inputs, eyes, and outputs
+    # todo
 
     return send_file(
         data,
